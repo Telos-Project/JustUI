bd4ccbcff7165ce97fda6603d0d42773d2d1cbba
Jesse Dinkin <kaeon.ace@gmail.com>
2025-06-28 03:56:51 -0400

    Code Updated

--- Code/JustUI.js
+++ Code/JustUI.js
@@ -394,7 +394,290 @@ var JustUI = {
 		}
 	},
 	utils: {
+		input: {
+			addInput: (element, input, onChange) => {
 
+				if(input == null) {
+
+					element.input = { };
+
+					input = element.input;
+				}
+
+				input = typeof input == "object" ? input : { };
+
+				input.pc = {
+					keyboard: [],
+					mouse: {
+						position: {
+							x: 0,
+							y: 0
+						},
+						buttons: {
+							left: false,
+							middle: false,
+							right: false
+						},
+						scroll: 0
+					}
+				};
+
+				onChange = onChange != null ? onChange : () => { };
+
+				element.addEventListener(
+					"keydown",
+					function(event) {
+
+						let previous = JSON.parse(JSON.stringify(input));
+						
+						if(!input.pc.keyboard.includes(
+							JustUI.utils.input.cleanKey(event.code)
+						)) {
+
+							input.pc.keyboard.push(
+								JustUI.utils.input.cleanKey(event.code)
+							);
+						}
+
+						onChange(previous, input);
+					}
+				);
+
+				element.addEventListener(
+					"keyup",
+					function(event) {
+
+						let previous = JSON.parse(JSON.stringify(input));
+
+						for(let i = 0; i < input.pc.keyboard.length; i++) {
+							
+							if(input.pc.keyboard[i] ==
+								JustUI.utils.input.cleanKey(event.code)) {
+
+								input.pc.keyboard.splice(i, 1);
+
+								i--;
+							}
+						}
+
+						onChange(previous, input);
+					}
+				);
+
+				element.addEventListener(
+					"mousedown",
+					function(event) {
+
+						let previous = JSON.parse(JSON.stringify(input));
+						
+						if(event.button == 0)
+							input.pc.mouse.buttons.left = true;
+						
+						if(event.button == 1)
+							input.pc.mouse.buttons.middle = true;
+						
+						if(event.button == 2)
+							input.pc.mouse.buttons.right = true;
+
+						onChange(previous, input);
+					}
+				);
+
+				element.addEventListener(
+					"mouseup",
+					function(event) {
+
+						let previous = JSON.parse(JSON.stringify(input));
+
+						if(event.button == 0)
+							input.pc.mouse.buttons.left = false;
+						
+						if(event.button == 1)
+							input.pc.mouse.buttons.middle = false;
+						
+						if(event.button == 2)
+							input.pc.mouse.buttons.right = false;
+
+						onChange(previous, input);
+					}
+				);
+
+				element.addEventListener(
+					"wheel",
+					function(event) {
+
+						let previous = JSON.parse(JSON.stringify(input));
+
+						input.pc.mouse.scroll = event.deltaY;
+
+						setTimeout(
+							function() {
+								input.pc.mouse.scroll = 0;
+							},
+							1000 / 60
+						);
+
+						onChange(previous, input);
+					}
+				);
+			},
+			cleanKey: (key) => {
+
+				return key.
+					split("Key").join("").
+					split("Digit").join("").
+					split("Arrow").join("").
+					toLowerCase();
+			}
+		},
+		speech: {
+			getVoices: () => {
+
+				let voices = window.speechSynthesis.getVoices();
+				let voiceList = [];
+
+				for(let i = 0; i < voices.length; i++)
+					voiceList.push(voices[i].voiceURI);
+
+				voiceList.sort();
+
+				if(voiceList.length > 0)
+					return voiceList;
+
+				return [
+					"Google Bahasa Indonesia",
+					"Google Deutsch",
+					"Google Nederlands",
+					"Google UK English Female",
+					"Google UK English Male",
+					"Google US English",
+					"Google español",
+					"Google español de Estados Unidos",
+					"Google français",
+					"Google italiano",
+					"Google polski",
+					"Google português do Brasil",
+					"Google русский",
+					"Google हिन्दी",
+					"Google 國語（臺灣）",
+					"Google 日本語",
+					"Google 한국의",
+					"Google 普通话（中国大陆）",
+					"Google 粤語（香港）",
+					"Microsoft David - English (United States)",
+					"Microsoft Mark - English (United States)",
+					"Microsoft Zira - English (United States)"
+				];
+			},
+			isSpeaking: () => {
+				return window.speechSynthesis.speaking;
+			},
+			listen: (callback) => {
+
+				callback = Array.isArray(callback) ? callback : [callback];
+
+				if(JustUI.utils.speech.recognition.listening)
+					JustUI.utils.speech.recognition.abort();
+
+				JustUI.utils.speech.recognition =
+					new webkitSpeechRecognition();
+
+				JustUI.utils.speech.recognition.continuous = true;
+				
+				JustUI.utils.speech.recognition.onresult = function(event) {
+
+					for(let i = 0; i < callback.length; i++) {
+
+						callback[i](
+							event.results[
+								event.results.length - 1
+							][0].transcript.trim()
+						);
+					}
+				}
+				
+				JustUI.utils.speech.recognition.onend = function(event) {
+
+					setTimeout(() => {
+						listen(callback);
+					}, 100);
+				}
+				
+				JustUI.utils.speech.recognition.start();
+
+				JustUI.utils.speech.recognition.listening = true;
+			},
+			recognition: (() => {
+
+				try {
+
+					window.speechSynthesis.getVoices();
+
+					let recognition = new webkitSpeechRecognition();
+
+					recognition.continuous = true;
+					recognition.listening = false;
+
+					return recognition;
+				}
+
+				catch(error) {
+					return null;
+				}
+			})(),
+			speak: (text, settings) => {
+
+				setTimeout(
+					function() {
+				
+						if(text.trim() == "")
+							return;
+					
+						settings = settings != null ? settings : { };
+
+						settings.voice =
+							settings.voice != null ?
+								settings.voice :
+								"Microsoft Mark - English (United States)";
+
+						settings.voice = settings.voice.trim().toLowerCase();
+					
+						let voices = window.speechSynthesis.getVoices();
+						let utterance = new SpeechSynthesisUtterance(text);
+					
+						for(let i = 0; i < voices.length; i++) {
+					
+							if(voices[i].voiceURI.toLowerCase() ==
+								settings.voice) {
+					
+								utterance.voice = voices[i];
+					
+								break;
+							}
+						}
+					
+						utterance.pitch =
+							settings.pitch != null ? settings.pitch : 1;
+
+						utterance.rate =
+							settings.rate != null ? settings.rate : 1;
+					
+						window.speechSynthesis.speak(utterance);
+					},
+					1
+				);
+			},
+			stopSpeaking: () => {
+				window.speechSynthesis.canel();
+			},
+			stopListening: () => {
+
+				if(JustUI.utils.speech.recognition.listening)
+					JustUI.utils.speech.recognition.abort();
+
+				JustUI.utils.speech.recognition.listening = false;
+			}
+		}
 	}
 };
 
